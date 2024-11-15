13301a07e0f0b3f6895b3b25ec5ec95de2a7e3a3
Max Hrebinets <brainisbro@Gmail.com>
2024-11-15 15:32:26 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,38 @@
+"use strict";
+const promise1 = new Promise((resolve, reject)=>{
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject();
+    }, 3000);
+});
+promise1.then(()=>{
+    const successMessage = document.createElement("div");
+    successMessage.classList.add("message");
+    successMessage.textContent = "Promise was resolved!";
+    document.body.appendChild(successMessage);
+}).catch(()=>{
+    const errorMessage = document.createElement("div");
+    errorMessage.classList.add("message", "error-message");
+    errorMessage.textContent = "Promise was rejected!";
+    document.body.appendChild(errorMessage);
+});
+// Обробник для promise2
+promise2.then(()=>{
+    const successMessage = document.createElement("div");
+    successMessage.classList.add("message");
+    successMessage.textContent = "Promise was resolved!";
+    document.body.appendChild(successMessage);
+}).catch(()=>{
+    const errorMessage = document.createElement("div");
+    errorMessage.classList.add("message", "error-message");
+    errorMessage.textContent = "Promise was rejected!";
+    document.body.appendChild(errorMessage);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
